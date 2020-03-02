8c141428b85cb3b3871931285de6560a5ab1a957
jagadishwarik <18wh5a0510@bvrithyderabad.edu.in>
2020-03-02 17:35:54 +0530

    created a new project

---
+++ calculator/src/main/java/calculator/Math.java
@@ -0,0 +1,18 @@
+package calculator;
+
+
+public class Math {
+	
+	double add(double x, double y) {
+		return x + y;
+	}
+	double substract(double x, double y) {
+		return x - y;
+	}
+	double multiply(double x, double y) {
+		return x * y;
+	}
+	double divide(double x, double y) {
+		return x / y;
+	}
+}
